392595fb3c5be1aabafd20c3a4151ef68b4fc188
Sarun Olankranok <Sarun.Olan@gmail.com>
2025-02-16 18:09:35 +0700

    update: contact page

--- src/components/Body/Dropdown.tsx
+++ src/components/Body/Dropdown.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import arrowUp from "../../assets/svg/arrow-up.svg";
-import arrowDown from "../../assets/svg/arrow-down.svg";
+import ArrowUp from "../../assets/svg/arrow-up.svg?react";
+import ArrowDown from "../../assets/svg/arrow-down.svg?react";
 interface IDropdown {
   options: string[];
   onSelect: (value: string) => void;
@@ -9,7 +9,7 @@ interface IDropdown {
 
 const Dropdown: React.FC<IDropdown> = ({ options, onSelect }) => {
   const [isOpen, setIsOpen] = useState(false);
-  const [selected, setSelected] = useState<string | null>(null);
+  const [selected, setSelected] = useState<string | null>(options[0]);
 
   const handleSelect = (option: string) => {
     setSelected(option);
@@ -20,13 +20,15 @@ const Dropdown: React.FC<IDropdown> = ({ options, onSelect }) => {
   return (
     <DropdownContainer>
       <DropdownButton onClick={() => setIsOpen(!isOpen)} isOpen={isOpen}>
-        <div>{selected || options[0]}</div>
-        <div>!</div>
+        <div>{selected}</div>
+        <div>{isOpen ? <ArrowUp /> : <ArrowDown />}</div>
       </DropdownButton>
       <DropdownList isOpen={isOpen}>
         {options.map((option, index) => (
           <DropdownItem key={index} onClick={() => handleSelect(option)}>
-            {option}
+            <div className={`${selected === option ? "selected" : ""}`}>
+              {option}
+            </div>
           </DropdownItem>
         ))}
       </DropdownList>
@@ -50,14 +52,17 @@ const DropdownButton = styled.button<{ isOpen: boolean }>`
   font-weight: bold;
   border: 1px solid white;
   color: ${({ isOpen }) => (isOpen ? "black" : "white")};
-  border-radius: ${({ isOpen }) =>
-    isOpen ? "5px 5px 0px 0px" : "5px 5px 5px 5px"};
+  border-radius: ${({ isOpen }) => (isOpen ? "5px 5px 0px 0px" : "5px")};
   cursor: pointer;
   z-index: 2;
   display: flex;
   justify-content: space-between;
   align-items: center;
   padding: 0px 4%;
+  & > div:nth-child(2) {
+    height: 100%;
+    width: 20px;
+  }
 `;
 
 const DropdownList = styled.ul<{ isOpen: boolean }>`
@@ -65,20 +70,27 @@ const DropdownList = styled.ul<{ isOpen: boolean }>`
   width: 100%;
   background: white;
   list-style: none;
-  padding: 0;
+  padding: 0px 0px 15px 0px;
   margin: 0;
-  border-radius: "0px 0px 5px 5px";
+  border-radius: 0px 0px 5px 5px;
   box-shadow: 0 6px 5px rgba(0, 0, 0, 0.2);
   display: ${({ isOpen }) => (isOpen ? "block" : "none")};
   z-index: 10;
 `;
 
 const DropdownItem = styled.li`
-  padding: 10px;
+  padding: 10px 4%;
   cursor: pointer;
   transition: background 0.3s;
   color: black;
-  &:hover {
-    background: #f1f1f1;
+  & > div {
+    padding: 5px 10px;
+    border: 1px solid #dadada;
+    border-radius: 8px;
+    font-weight: bold;
+    font-size: 0.8rem;
+    &.selected {
+      border: 1px solid black;
+    }
   }
 `;
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -2,8 +2,35 @@ import { styled } from "styled-components";
 import workingVideo from "../assets/video/working.mp4";
 import Footer from "../components/Footer/Footer";
 import Dropdown from "../components/Body/Dropdown";
+import { useState } from "react";
 const EMAIL_CLIENT_OPTIONS = ["Default Email App", "Gmail", "Outlook"];
 const Contact = () => {
+  const email = "sarun.olan@gmail.com";
+  const [selectedClient, setSelectedClient] = useState(EMAIL_CLIENT_OPTIONS[0]);
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+  const handleOpenEmailClient = () => {
+    let emailLink = "";
+
+    switch (selectedClient) {
+      case "Gmail":
+        emailLink = `https://mail.google.com/mail/?view=cm&fs=1&to=${email}&su=${encodeURIComponent(
+          subject
+        )}&body=${encodeURIComponent(message)}`;
+        break;
+      case "Outlook":
+        emailLink = `https://outlook.live.com/mail/0/deeplink/compose?to=${email}&subject=${encodeURIComponent(
+          subject
+        )}&body=${encodeURIComponent(message)}`;
+        break;
+      default:
+        emailLink = `mailto:${email}?subject=${encodeURIComponent(
+          subject
+        )}&body=${encodeURIComponent(message)}`;
+    }
+
+    window.open(emailLink, "_blank");
+  };
   return (
     <SContainer>
       <div>
@@ -19,28 +46,107 @@ const Contact = () => {
           <div>
             <div>SEND FORM</div>
             <div>
-              <Dropdown options={EMAIL_CLIENT_OPTIONS} onSelect={() => {}} />
+              <Dropdown
+                options={EMAIL_CLIENT_OPTIONS}
+                onSelect={setSelectedClient}
+              />
             </div>
           </div>
           <div>
             <div>TO</div>
+            <SMail>
+              <div>sarun.olan@gmail.com</div>
+            </SMail>
+          </div>
+          <div>
+            <div>SUBJECT</div>
             <div>
-              <input value={"sarun.olan@gmail.com"} />
+              <SInput
+                value={subject}
+                onChange={(e) => {
+                  setSubject(e.target.value);
+                }}
+                placeholder="Please input subject"
+              />
             </div>
           </div>
           <div>
             <div>MESSAGE</div>
             <div>
-              <textarea />
+              <STextArea
+                value={message}
+                onChange={(e) => {
+                  setMessage(e.target.value);
+                }}
+              />
             </div>
           </div>
+          <SButton onClick={() => handleOpenEmailClient()}>
+            <div>Send</div>
+          </SButton>
         </SMailContainer>
-        <Footer />
       </div>
+      <Footer />
     </SContainer>
   );
 };
 export default Contact;
+const SButton = styled.div`
+  background: white;
+  color: black;
+  display: flex;
+  justify-content: center !important;
+  align-items: center !important;
+  border-radius: 15px;
+  padding: 0 !important;
+  margin: 30px 0px !important;
+  cursor: pointer;
+  & > div {
+    font-size: 25px !important;
+    width: fit-content !important;
+    margin: 0 !important;
+    padding: 0 !important;
+  }
+`;
+const SInput = styled.input`
+  width: 95%;
+  height: 100%;
+  background: rgba(255, 255, 255, 0.1);
+  color: white;
+  border-radius: 5px;
+  border: 1px solid white;
+  display: flex;
+  align-items: center;
+  padding-left: 5%;
+  font-weight: bold;
+  &:focus {
+    outline: none;
+  }
+`;
+const STextArea = styled.textarea`
+  width: 100%;
+  min-height: 100px;
+  background: rgba(255, 255, 255, 0.1);
+  color: white;
+  resize: none;
+  overflow: auto;
+  border-radius: 5px;
+  &:focus {
+    outline: none;
+  }
+`;
+const SMail = styled.div`
+  width: 100%;
+  border: 1px solid white;
+  border-radius: 5px;
+  display: flex;
+  align-items: center;
+  background: rgba(255, 255, 255, 0.1);
+  & > div {
+    margin-left: 5%;
+    font-weight: bold;
+  }
+`;
 const SMailContainer = styled.div`
   padding: 0px 20%;
   & > div {
